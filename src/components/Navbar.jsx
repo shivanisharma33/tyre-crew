40fbda501c68274cbc84566752ab3f8f43aac7ce
shividixit <shivani.midis@gmail.com>
2026-03-30 15:27:29 +0530

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
       >
         {/* Logo */}
         <Link to="/" className="flex items-center h-full py-2 sm:py-3 w-1/2 sm:w-auto">
-          <img src="/images/mtc-logo-new.png" alt="Mobile Tyre Champions" className="h-full w-auto max-w-full object-contain" />
+          <img src="/images/Asset 1.png" alt="Mobile Tyre Champions" className="h-full w-auto max-w-full object-contain" />
         </Link>
 
         {/* Desktop Links */}
